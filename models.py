082783427a98025c2998c384fbf0dbe08cd81ea7
lordYorden <87636389+lordYorden@users.noreply.github.com>
2025-12-27 17:57:06 +0200

    added relevent notif info db and check exsisting by hash

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
-from typing import Optional
+from typing import Optional, List
+from sqlalchemy import Column, JSON
 
 class BaseSmsMessage(SQLModel):
     phone_number: str
@@ -12,9 +13,22 @@ class SmsMessage(BaseSmsMessage, table=True):
     
 class BaseNotification(SQLModel):
     title: str
+    extraTitle: Optional[str]
+    isGroup: Optional[bool]
     body: Optional[str]
     timestamp: Optional[int]
     packageName: Optional[str]
     
 class Notification(BaseNotification, table=True):
     id: str = Field(default=None, primary_key=True)
+    
+class BaseReleventInfo(SQLModel):
+    body: Optional[str]
+    packageName: Optional[str]
+    hash: Optional[str]
+    urls: Optional[List[str]] = Field(default=None, sa_column=Column(JSON))
+class ReleventInfo(BaseReleventInfo, table=True):
+    id: str = Field(default=None, primary_key=True)
+    
+
+    
